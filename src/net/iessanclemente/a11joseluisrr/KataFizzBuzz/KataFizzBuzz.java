2e872a8332969de282030dc1896f8ff1da85bd99
regorodrijl <regorodrijl@gmail.com>
2016-05-13 14:58:07 +0200

    añadido test - Si se no se introduce nada devuelve 0 (added test not enter anything return 0)

--- src/net/iessanclemente/a11joseluisrr/KataFizzBuzz/KataFizzBuzz.java
+++ src/net/iessanclemente/a11joseluisrr/KataFizzBuzz/KataFizzBuzz.java
@@ -2,4 +2,11 @@ package net.iessanclemente.a11joseluisrr.KataFizzBuzz;
 
 public class KataFizzBuzz {
 
+	public int number(String txt) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
 }
