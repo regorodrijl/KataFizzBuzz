35764dd30fb12b3caf40e6ef213f0d902b547163
regorodrijl <regorodrijl@gmail.com>
2016-05-15 00:03:12 +0200

    añadido test - si tiene un 5 o es divisible por 5 devuelve buzz

--- newRequest3/net/iessanclemente/a11joseluisrr/Test/TestKataFizzBuzz.java
+++ newRequest3/net/iessanclemente/a11joseluisrr/Test/TestKataFizzBuzz.java
@@ -15,7 +15,7 @@ public class TestKataFizzBuzz {
 	@Before
 	public void setUp() throws Exception {
 		fb = new KataFizzBuzz();
-		//r3 = new newRequest3();
+		// r3 = new newRequest3();
 	}
 
 	/**
@@ -60,28 +60,28 @@ public class TestKataFizzBuzz {
 	}
 
 	/**
-	 * testIntroducirNumeroDivisiblePorTresDevuelveFizz
-	 * Introducir un numero divisible por 3 devuelve Fizz
+	 * testIntroducirNumeroDivisiblePorTresDevuelveFizz Introducir un numero
+	 * divisible por 3 devuelve Fizz
 	 */
 	@Test
 	public void testIntroducirNumeroDivisiblePorTresDevuelveFizz() {
 		String returnFizz = fb.number(12);
 		assertEquals("Fizz", returnFizz);
 	}
-	
+
 	/**
-	 * testIntroducirNumeroDivisiblePorTresDevuelveFizz
-	 * Introducir un numero divisible por 5 devuelve Buzz
+	 * testIntroducirNumeroDivisiblePorTresDevuelveFizz Introducir un numero
+	 * divisible por 5 devuelve Buzz
 	 */
 	@Test
 	public void testIntroducirNumeroDivisiblePorCincoDevuelveBuzz() {
 		String returnBuzz = fb.number(10);
 		assertEquals("Buzz", returnBuzz);
 	}
-	
+
 	/**
-	 * testIntroducirNumeroDivisiblePorTresYCincoDevuelveFizzBuzz
-	 * Introducir un numero divisible por 3 y 5 devuelve FizzBuzz
+	 * testIntroducirNumeroDivisiblePorTresYCincoDevuelveFizzBuzz Introducir un
+	 * numero divisible por 3 y 5 devuelve FizzBuzz
 	 */
 	@Test
 	public void testIntroducirNumeroDivisiblePorTresYCincoDevuelveFizzBuzz() {
@@ -90,13 +90,23 @@ public class TestKataFizzBuzz {
 	}
 
 	/**
-	 * testRequest3esDivisiblePor3Ytiene3DevueveFizz
-	 * Un número es Fizz si es divisible por 3 o si incluye un 3 en el número
-	 * comprobado con el numero 33, 13 y el 4356 por cercionarme.
+	 * testRequest3esDivisiblePor3Ytiene3DevueveFizz Un número es Fizz si es
+	 * divisible por 3 o si incluye un 3 en el número comprobado con el numero
+	 * 33, 13 y el 4356 por cercionarme.
 	 */
 	@Test
 	public void testRequest3esDivisiblePor3Ytiene3DevueveFizz() {
 		String returnFizz = fb.number(13); // probado con 33
 		assertEquals("Fizz", returnFizz);
+	}
+
+	/**
+	 * testRequest5esDivisiblePor5oTiene5DevueveBuezz Un número es Buzz si es
+	 * divisible por 5 o si incluye un 5 en el número
+	 */
+	@Test
+	public void testRequest5esDivisiblePor5oTiene5DevueveBuezz() {
+		String returnBuzz = fb.number(53);
+		assertEquals("Buzz", returnBuzz);
 	}
 }
